22742bedc548b8ffd9646bdb2a88fc9594598076
violettomania <danielpolya@protonmail.com>
2024-01-13 12:05:36 +0100

    feature: search during user typing

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,20 @@
+import { useAppDispatch } from '../store/store';
+import { fetchCocktails } from '../actions/fetchCocktails';
+
 export default function Search() {
+  const dispatch = useAppDispatch();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchTerm = e.target.value;
+    dispatch(fetchCocktails(newSearchTerm));
+  };
+
   return (
     <section className='section search'>
       <form className='search-form'>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
-          <input type='text' name='name' id='name' />
+          <input type='text' name='name' id='name' onChange={handleChange} />
         </div>
       </form>
     </section>
